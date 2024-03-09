e516290f52e8ed9a0044beeac01705b1ea23a50f
chhzh123 <hc676@cornell.edu>
2024-03-09 16:13:34 -0500

    [dahlia] Update report

--- polybench/dahlia/correlation/correlation.cpp
+++ polybench/dahlia/correlation/correlation.cpp
@@ -1,6 +1,6 @@
 #include <ap_int.h>
 extern "C" {
-  void kernel(float float_n, float data[260][240], float mean[240], float stddev[240], float cov[240][240], float corr[240][240]) {
+  void kernel(float float_n, float data[260][240], float mean[240], float stddev[240], float corr[240][240]) {
     #pragma HLS INTERFACE s_axilite port=float_n bundle=control
     #pragma HLS INTERFACE m_axi port=data offset=slave bundle=gmem
     #pragma HLS INTERFACE s_axilite port=data bundle=control
@@ -8,8 +8,6 @@ extern "C" {
     #pragma HLS INTERFACE s_axilite port=mean bundle=control
     #pragma HLS INTERFACE m_axi port=stddev offset=slave bundle=gmem
     #pragma HLS INTERFACE s_axilite port=stddev bundle=control
-    #pragma HLS INTERFACE m_axi port=cov offset=slave bundle=gmem
-    #pragma HLS INTERFACE s_axilite port=cov bundle=control
     #pragma HLS INTERFACE m_axi port=corr offset=slave bundle=gmem
     #pragma HLS INTERFACE s_axilite port=corr bundle=control
     #pragma HLS INTERFACE s_axilite port=return bundle=control
